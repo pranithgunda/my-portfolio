11ef4cf1f2d2d5c90d5575c759581ed7ba8dfdd6
Pranith Gunda <pranith0527@yahoo.com>
2024-08-11 19:04:14 -0400

    Added Contact  Form fields and validation logic for fields to be not null

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,59 @@
 // Contact form component
 
-function Contact(){
-    return(
-        <div className="p-4">
-        <h4>Contact</h4>
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import React, { useEffect, useState } from 'react';
 
+
+function Contact() {
+
+    const [formFields, setFormFields] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+    const [error, setError] = useState('');
+
+    const handleInputChange = (event) => {
+        event.preventDefault();
+        const { name, value } = event.target;
+        if (value !== "") {
+            // set form fields
+            setFormFields((prevFormFields) => ({
+                ...prevFormFields,
+                [name]: value,
+            }));
+            setError('');
+        } else {
+            // if empty reset value to null
+            setFormFields((prevFormFields) => ({
+                ...prevFormFields,
+                [name]: '',
+            }));
+            const errorMessage = 'Value cannot be blank';
+            setError(errorMessage);
+        }
+    }
+
+    return (
+        <div className="p-4" style={{ width: '75%' }}>
+            <h4>Contact</h4>
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control type="text" name="name" value={formFields.name} onChange={handleInputChange} placeholder='Enter name' />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name="email" value={formFields.email} onChange={handleInputChange} placeholder="name@example.com" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control as='textarea' name="message" value={formFields.message} onChange={handleInputChange} rows={3} />
+                    {error && <p>{error}</p>}
+                </Form.Group>
+                <Button variant="primary">Submit</Button>
+            </Form>
         </div>
     )
 };
